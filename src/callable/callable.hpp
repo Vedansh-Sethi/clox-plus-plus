0bff235b70d9c221615b83584e86c13d9945b655
Vedansh-Sethi <vedansh25sethi@gmail.com>
2026-03-19 12:24:04 +0000

    Minor fix

--- src/callable/callable.hpp
+++ src/callable/callable.hpp
@@ -7,7 +7,7 @@ class Interpreter;
 class Callable
 {
 public:
-    virtual int arity() const = 0;
+    virtual int arity() = 0;
     virtual LiteralValue call(Interpreter* interpreter, std::vector<LiteralValue> arguments) = 0;
     virtual std::string toString() const = 0;
 };
